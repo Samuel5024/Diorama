e41bf8bd538a83253543864d22317a9bd45c0913
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2026-01-22 15:28:30 -0700

    Update LookMoveTo.cs, create KillTarget.cs, adjust and rebake navmesh

--- M1_DV1_Diorama/Assets/Scripts/LookMoveTo.cs
+++ M1_DV1_Diorama/Assets/Scripts/LookMoveTo.cs
@@ -7,21 +7,23 @@ public class LookMoveTo : MonoBehaviour
     void Update()
     {
         Ray ray;
-        RaycastHit hit;
+        RaycastHit[] hits;
         GameObject hitObject;
 
         Debug.DrawRay(camera.position, camera.rotation * Vector3.forward * 100.0f);
         ray = new Ray(camera.position, camera.rotation * Vector3.forward);
+        hits = Physics.RaycastAll(ray);
 
-        if(Physics.Raycast(ray, outhit))
+        for(int i = 0; i < hits.Length; i++)
         {
+            RaycastHit hit = hits[i];
             hitObject = hit.collider.gameObject;
             if(hitObject == ground)
             {
                 Debug.Log("Hit (x,y,z): " + hit.point.ToString("F2"));
                 transform.position = hit.point;
             }
-        }     
+        }
     }
 
     private Transform camera;
